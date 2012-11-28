a2be86fade087b144a63e847e4da9567248fc6ba
offense74 <magnus.eliasson1@gmail.com>
2012-11-28 11:02:16 +0100

    Initial Commit

---
+++ munkidirectory/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  munkidirectory
+//
+//  Created by Magnus Eliasson on 11/28/12.
+//  Copyright (c) 2012 Magnus Eliasson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
