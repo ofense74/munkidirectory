afcd0b6527709baad57677bf63a52632df374286
Magnus Eliasson <xelima@mdse3179390.gu.gu.se>
2013-02-25 17:29:00 +0100

    Added embryo for Apps and Manifest window

---
+++ AD Munki Server/AD Munki Server/AppsManifestsWindowController.h
@@ -0,0 +1,13 @@
+//
+//  AppsManifestsWindowController.h
+//  AD Munki Server
+//
+//  Created by Magnus Eliasson on 2/25/13.
+//  Copyright (c) 2013 Magnus Eliasson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppsManifestsWindowController : NSWindowController
+
+@end
