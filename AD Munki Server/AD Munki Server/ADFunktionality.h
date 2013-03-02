da7c6b1cd938bca0d0134b5c009b22e69112c29a
ofense74 <magnus.eliasson1@gmail.com>
2013-03-02 22:25:42 +0100

    Added window
    Added window for AD functionality for manifests

---
+++ AD Munki Server/AD Munki Server/ADFunktionality.h
@@ -0,0 +1,13 @@
+//
+//  ADFunktionality.h
+//  AD Munki Server
+//
+//  Created by Magnus E on 2/3/13.
+//  Copyright (c) 2013 Magnus Eliasson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ADFunktionality : NSWindowController
+
+@end
