6142478dfcd20bdc2a4bdcde393d76e2baa770ba
ofense74 <magnus.eliasson1@gmail.com>
2013-03-02 23:06:00 +0100

    Added window for manifests
    Added a window for adding and removing ad functionality to other
    manifests.

---
+++ AD Munki Server/AD Munki Server/ManifestAddWindow.h
@@ -0,0 +1,13 @@
+//
+//  ManifestAddWindow.h
+//  AD Munki Server
+//
+//  Created by Magnus E on 2/3/13.
+//  Copyright (c) 2013 Magnus Eliasson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ManifestAddWindow : NSWindowController
+
+@end
---
+++ AD Munki Server/AD Munki Server/ManifestAddWindow.m
@@ -0,0 +1,34 @@
+//
+//  ManifestAddWindow.m
+//  AD Munki Server
+//
+//  Created by Magnus E on 2/3/13.
+//  Copyright (c) 2013 Magnus Eliasson. All rights reserved.
+//
+
+#import "ManifestAddWindow.h"
+
+@interface ManifestAddWindow ()
+
+@end
+
+@implementation ManifestAddWindow
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
